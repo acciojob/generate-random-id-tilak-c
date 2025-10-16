07b504e92dc1201c2bfa86102370d12e368bc81c
tilak-c <66167345+tilak-c@users.noreply.github.com>
2025-10-16 20:13:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function makeid(l) {
-  // write your code here
+   const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let result = "";
+    for (let i = 0; i < l; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        result += characters[randomIndex];
+    }
+    return result;
+	
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
+// const l = prompt("Enter a number.");
 alert(makeid(l));
